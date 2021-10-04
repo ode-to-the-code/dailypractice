bb49d94872149c92357bf2bb0e39f7659ee95c0a
jad abdallah <justcallmethis@gmail.com>
2021-10-04 10:02:26 -0700

    adding smallerNumberThanCurrent file

---
+++ smallerNumberThanCurrent-10-4.js
@@ -0,0 +1,29 @@
+
+
+
+// input nums=[8,1,2,2,3]
+// sorted = [1, 2, 2, 3, 8]
+// output array= [4,0,1,1,3]
+
+const smallerNumbersThanCurrent = (nums) => {
+    let smallerNumCount = 0;
+    let outputHash = {};
+
+
+
+
+}
+
+// nums = [1, 3, 2]
+const jadSort = (nums) => {
+    // loop over array 
+    // let sorted = [];
+
+    if (var i = 0; i < nums.length; i++) {
+
+    }
+}
+
+// hw
+// do bubblesort for jadsort (to use on the smallerNumberThanCurrent)
+// do the codeacademy quizzes, try before wednesday 
