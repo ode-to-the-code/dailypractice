f0d75fce6a880a0cfc806a355a5103c0b93e5470
jad abdallah <justcallmethis@gmail.com>
2021-10-06 10:02:17 -0700

    time complexity, sorting, continued work on smallerNumberThanCurrent. finish out this function steps 2 and 3, and try some implementation of bububble sort. also finish codeacademy functions quiz/section. Talked about cloning arrays, and the === vs == signs.

--- smallerNumberThanCurrent-10-4.js
+++ smallerNumberThanCurrent-10-4.js
@@ -1,29 +1,76 @@
 
 
 
-// input nums=[8,1,2,2,3]
+// input nums=[8,1,2,2,3] 
 // sorted = [1, 2, 2, 3, 8]
+//           0  1  2  3  4
 // output array= [4,0,1,1,3]
 
+const arr = [1, 2, 3, 4];
+const arr1 = arr;
+const arr2 = [...arr]; // this is the ES6 way of cloning an array 
+console.log(arr === arr1); // this one is true, bc they're pointing to the same memory space 
+console.log(arr === arr2); // but this one will return false, bc they're pointing to different places in the memory
+
+
+
+// hw finish functions quiz in 
+
 const smallerNumbersThanCurrent = (nums) => {
-    let smallerNumCount = 0;
-    let outputHash = {};
+    // step 1: clone the array and sort the cloned array 
+    let sorted = [...nums];
+    sorted.sort(function(a, b) { return a - b});
 
+    // step 2: map the number to output, which will be the indexes 
+    let dict = {};
+
+    for (var i = 0; i < sorted.length)
 
 
 
 }
 
-// nums = [1, 3, 2]
-const jadSort = (nums) => {
-    // loop over array 
-    // let sorted = [];
 
-    if (var i = 0; i < nums.length; i++) {
+// // nums = [1, 3, 2]
+// const jadSort = (nums) => {
+//     // loop over array 
+//     // let sorted = [];
 
-    }
-}
+//     if (var i = 0; i < nums.length; i++) {
 
-// hw
-// do bubblesort for jadsort (to use on the smallerNumberThanCurrent)
-// do the codeacademy quizzes, try before wednesday 
+//     }
+// }
+
+// // hw
+// // do bubblesort for jadsort (to use on the smallerNumberThanCurrent)
+// // do the codeacademy quizzes, try before wednesday 
+
+
+
+
+
+// let bubbleSort = (inputArr) => {
+//     let len = inputArr.length;
+//     for (let i = 0; i < len; i++) {
+//         for (let j = 0; j < len; j++) {
+//             if (inputArr[j] > inputArr[j + 1]) {
+//                 let tmp = inputArr[j];
+//                 inputArr[j] = inputArr[j + 1];
+//                 inputArr[j + 1] = tmp;
+//             }
+//         }
+//     }
+//     return inputArr;
+// };
+
+
+
+
+// let bubbleSort = (inputArray) => {
+//     let len = inputArr.length; 
+//     for (let i = 0; i < len; i++) {
+//         for (let j = 0; j < len; j++) {
+//             if (inputArr[j])
+//         }
+//     }
+// }
